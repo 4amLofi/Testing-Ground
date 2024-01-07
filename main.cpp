82b372e51792566793fa47bb2019b2d3871c1a99
Lofi <129073732+4amLofi@users.noreply.github.com>
2024-01-06 20:44:55 -0700

    Ball dropped from tower

--- main.cpp
+++ main.cpp
@@ -1,40 +1,46 @@
-#include <iostream>
+#include <iostream> // Required for std::cout and std::cin
 
-double getDouble()
+// Calculate the height of the ball given the towers height, after x seconds, and return value to caller.
+double calculateBallHeight(double towerHeight, short seconds)
 {
-    std::cout << "Enter a double value: ";
-    double d{};
-    std::cin >> d;
-    return d;
+    double ballHeight{towerHeight - (9.8 * (seconds * seconds)) / 2};
+    return ballHeight;
 }
 
-char getOperator()
+// Print the current height of the ball if above the ground, or print ball is on ground if ball's height not above 0.
+void printBallHeight(double towerHeight, short seconds)
 {
-    std::cout << "Enter +, -, *, or /: ";
-    char c{};
-    std::cin >> c;
-    return c;
+    // Calculate the ball's height and store to ballHeight
+    double ballHeight{calculateBallHeight(towerHeight, seconds)};
+    
+    if (ballHeight > 0)
+        std::cout << "At " << seconds << " seconds, the ball is at height: " << ballHeight << " meters\n";
+    else
+        std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
 }
 
-void printResults(double d1, double d2, char c)
+// Multiple function calls to printBallHeight() with seconds ranging from 0 through 5 (for loop not introduced yet).
+void functionCalls(double towerHeight)
 {
-    if (c == '+')
-        std::cout << d1 << " + " << d2 << " is " << d1 + d2 << '\n';
-    else if (c == '-')
-        std::cout << d1 << " - " << d2 << " is " << d1 - d2 << '\n';
-    else if (c == '*')
-        std::cout << d1 << " * " << d2 << " is " << d1 * d2 << '\n';
-    else if (c == '/')
-        std::cout << d1 << " / " << d2 << " is " << d1 / d2 << '\n';
+    printBallHeight(towerHeight, 0);
+    printBallHeight(towerHeight, 1);
+    printBallHeight(towerHeight, 2);
+    printBallHeight(towerHeight, 3);
+    printBallHeight(towerHeight, 4);
+    printBallHeight(towerHeight, 5);
+}
+
+// Get the height of the tower from the user and return to caller.
+double getTowerHeight()
+{
+    std::cout << "Enter the height of the tower in meters: ";
+    double towerHeight{};
+    std::cin >> towerHeight;
+    return towerHeight;
 }
 
 int main()
 {
-    double d1{getDouble()};
-    double d2{getDouble()};
-    char c{getOperator()};
-
-    printResults(d1, d2, c);
-
-    return 0;
+    // Get tower height, and pass along to functionCalls().
+    functionCalls(getTowerHeight());
 }
