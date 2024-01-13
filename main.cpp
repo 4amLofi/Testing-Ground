e5250336cf508d1ebfbe47e8576936d1e2fca8e9
Lofi <129073732+4amLofi@users.noreply.github.com>
2024-01-12 22:16:44 -0700

    consteval and constexpr

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <type_traits>
 
+consteval auto compileTime(auto value)
+{
+    return value;
+}
+
 constexpr bool isConstantEvaluated()
 {
     return std::is_constant_evaluated();
@@ -8,9 +13,15 @@ constexpr bool isConstantEvaluated()
 
 int main()
 {
-    constexpr bool x{isConstantEvaluated()};
+    constexpr bool x{compileTime(isConstantEvaluated())};
+    std::cout << std::boolalpha << "value x is_constantly_evaluated(): " << x << '\n';
 
-    std::cout << std::boolalpha << "is_constantly_evaluated(): " << x << '\n';
+    bool y{compileTime(isConstantEvaluated())};
+    std::cout << std::boolalpha << "value y is_constantly_evaluated(): " << y << '\n';
+
+    std::cout << std::boolalpha << "consteval literal is_constantly_evaluated(): " << compileTime(isConstantEvaluated()) << '\n';
+
+    std::cout << std::boolalpha << "literal is_constantly_evaluated(): " << isConstantEvaluated() << '\n';
 
     return 0;
 }
